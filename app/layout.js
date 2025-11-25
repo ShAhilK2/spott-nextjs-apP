30e20a3d25980478f561e0dba553943aa6cc2cb5
ShAhilK2 <shahilkataria852002@gmail.com>
2025-11-25 13:13:08 +0530

    feat : Create App Layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "@/components/header";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,11 +9,39 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+          {/* Header */}
+          <Header />
+          <main className="min-h-screen relative container mx-auto pt-40 md:pt-32">
+            {/* glow */}
+            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+              <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"/>
+
+              <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"/>
+           
+            </div>
+            <div className="relative z-10 min-h-[90vh]">
+              {children}
+            </div>
+
+            {/* footer */}
+            <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+            <div className="text-sm text-gray-400">
+              Made with ❤️ by Shahil Kataria
+            </div>
+            </footer>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
