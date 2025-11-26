eedab2dff518b3accab40a25c6bf82a7f1c85d35
ShAhilK2 <shahilkataria852002@gmail.com>
2025-11-27 02:51:34 +0530

    feat : Storing User in Database

---
+++ convex/schema.js
@@ -0,0 +1,45 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+
+    // Users Table 
+    users: defineTable({
+        name: v.string(),
+        tokenIdentifier: v.string(),   //Clerk user Id for auth
+        email : v.string(),
+        imageUrl : v.optional(v.string()),
+
+
+        // OnBoarding 
+
+        hasCompletedOnBoarding : v.boolean(),
+        location : v.optional(v.object({
+            city : v.string(),
+            state : v.optional(v.string()),
+            country : v.string(),
+            
+        })),
+
+
+        interests : v.optional(v.array(v.string())), // Minimum 3 Categeories
+
+        // Organizer  tracking (User Subscription)
+
+        freeEventsCreated  :v.number(),   //Track free event limit (1 free)
+
+
+        // Timestamps 
+
+
+        createdAt : v.number(),
+        updatedAt : v.number(),
+
+
+
+
+        
+
+
+      }).index("by_token", ["tokenIdentifier"]),
+})
